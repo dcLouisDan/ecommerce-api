cdabd40b562faa25a79e04cd94ca9ed7bed1b342
Dan Louis Dela Cruz <louisdc002@gmail.com>
2024-06-14 16:18:28 +0800

    Create user registration function

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/dclouisDan/ecommerce-api/cmd/service/user"
+	"github.com/dclouisDan/ecommerce-api/service/user"
 	"github.com/gorilla/mux"
 )
 
@@ -22,13 +22,14 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 }
 
 func (s *APIServer) Run() error {
-  router := mux.NewRouter()
-  subrouter := router.PathPrefix("/api/v1").Subrouter()
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
 
-  userHandler := user.NewHandler()
-  userHandler.RegisterRoutes(subrouter)
+	userStore := user.NewStore(s.db)
+	userHandler := user.NewHandler(userStore)
+	userHandler.RegisterRoutes(subrouter)
 
-  log.Println("Listening on", s.addr)
+	log.Println("Listening on", s.addr)
 
-  return http.ListenAndServe(s.addr, router)
+	return http.ListenAndServe(s.addr, router)
 }
--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.24.0
 )
